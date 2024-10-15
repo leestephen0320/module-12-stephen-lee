32227d7f0824661c8620b0dcf4c5baa854401af5
Stephen Lee <leestephen0320@gmail.com>
2024-10-15 02:02:12 -0700

    added code template

---
+++ project/src/App.jsx
@@ -0,0 +1,17 @@
+// Todo: Bring in the required import from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <>
+      <Nav />
+      {/* Todo: Fill in the appropriate component so that the user will conditionally see the appropriate view based on the URL */}
+      <main className="mx-3">
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ project/src/main.jsx
@@ -0,0 +1,39 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import AboutPage from './pages/AboutPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: '/ProfilePage/:id',
+        element: <ProfilePage />
+      },
+      {
+        path: '/About',
+        element: <AboutPage />
+      },
+    ]
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
